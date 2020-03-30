be99fe3d1964b7413ec01074e814288e4a42c6c6
Nicolas HAHN <nicolas.hahn@bluesoft-group.com>
2020-03-30 22:30:32 +0200

    add: DevicesScreen

---
+++ src/plantwatering/lib/features/devices/DevicesScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DevicesScreen extends StatelessWidget {
+
+  static Route<dynamic> route() {
+    return MaterialPageRoute(builder: (context) => DevicesScreen());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
